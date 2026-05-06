8d2f7839e695b1c79ca936735cbe1a511a7fc245
ksovreli <sandroqsovreli@gmail.com>
2026-05-07 02:32:54 +0400

    fixed lag issues and make ui better and more attractive.

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener, Inject, PLATFORM_ID, signal } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, signal, NgZone, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { fromEvent, Subscription } from 'rxjs';
+import { throttleTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +11,40 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-  // ვიყენებთ signals-ს უკეთესი პერფორმანსისთვის
+export class Header{
   isMenuOpen = signal(false);
   isScrolled = signal(false);
   isBrowser: boolean;
+  private scrollSub?: Subscription;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    // ვამოწმებთ, გაშვებულია თუ არა კოდი ბრაუზერში
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private ngZone: NgZone // ვიყენებთ NgZone-ს
+  ) {
     this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit() {
+    if (this.isBrowser) {
+        this.ngZone.runOutsideAngular(() => {
+        this.scrollSub = fromEvent(window, 'scroll')
+          .pipe(
+            throttleTime(20),
+            map(() => window.scrollY > 50)
+          )
+          .subscribe((scrolled) => {
+            if (this.isScrolled() !== scrolled) {
+              this.ngZone.run(() => {
+                this.isScrolled.set(scrolled);
+              });
+            }
+          });
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.scrollSub?.unsubscribe();
   }
 
   toggleMenu() {
@@ -26,13 +53,5 @@ export class Header {
 
   closeMenu() {
     this.isMenuOpen.set(false);
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    // კრიტიკული ფაქტორი: window-ს ვიყენებთ მხოლოდ ბრაუზერში
-    if (this.isBrowser) {
-      this.isScrolled.set(window.scrollY > 50);
-    }
   }
 }
